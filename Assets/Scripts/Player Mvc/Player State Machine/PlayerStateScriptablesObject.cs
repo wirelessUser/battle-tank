416f5d1d9ba492662c050bb89688587e788f6a33
workspacetodo <workspacetodo@gmail.com>
2023-09-08 21:42:42 +0530

    Merge Conflict issue

---
+++ Assets/Scripts/Player Mvc/Player State Machine/PlayerStateScriptablesObject.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName= "ScriptableObject", menuName = "ScriptableObject/TanksStates")]
+public class PlayerStateScriptablesObject : ScriptableObject
+{
+    public float walkSpeed;
+   
+    //***#### Becuase Run,walk rotate,Shoott Dies all things are States of playe then why We need to create Model for player 
+
+
+}
